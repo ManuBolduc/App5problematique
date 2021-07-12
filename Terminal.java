fc8e2b120056144499ebf153777946c41e7e7704
Emmanuel Bolduc <emmanuel.bolduc@usherbrooke.ca>
2021-07-12 18:10:48 -0400

    missing files

---
+++ Terminal.java
@@ -0,0 +1,29 @@
+package APP5;
+
+/** @author Ahmed Khoumsi */
+
+/** Cette classe identifie les terminaux reconnus et retournes par
+ *  l'analyseur lexical
+ */
+public class Terminal {
+  public String chaine;
+
+
+
+
+
+
+
+
+
+
+
+/** Un ou deux constructeurs (ou plus, si vous voulez)
+  *   pour l'initalisation d'attributs
+ * @param o_chaine
+ */	
+  public Terminal(String o_chaine) {   // arguments possibles
+     //
+  }
+
+}
